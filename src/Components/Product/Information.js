d4c39012a93215cad4ec1d4cee1809f00df16b01
quangdm <qduongminh3@gmail.com>
2021-07-17 00:45:33 +0700

    Add Information and RelatedProduct to ProductPage

--- src/Components/Product/Information.js
+++ src/Components/Product/Information.js
@@ -1,6 +1,13 @@
 import { React, useState } from "react";
-import styles from "Assets/Stylesheets/SCSS/Pages/ProductPage.module.scss";
+import styles from "Assets/Stylesheets/SCSS/Components/Information.module.scss";
 import ReactStars from "react-rating-stars-component";
+const iconURL = [
+  "https://d13wriz42ny3t5.cloudfront.net/production/2017/06/23231934/icons_b-corp.png",
+  "https://d13wriz42ny3t5.cloudfront.net/production/2017/06/23231926/icons_sustainable-lifestyle.png",
+  "https://d13wriz42ny3t5.cloudfront.net/production/2017/06/23231933/icons_charitable.png",
+  "https://d13wriz42ny3t5.cloudfront.net/production/2017/07/23231914/bpa_free.png",
+  "https://d13wriz42ny3t5.cloudfront.net/production/2017/06/23231930/icons_made-in-the-usa.png",
+];
 export default function ProductInformation() {
   const [NumItem, setNumItem] = useState("1");
   return (
@@ -12,49 +19,58 @@ export default function ProductInformation() {
         />
       </div>
 
-      <div className={styles.information}>
-        <div>
-          <h1 className={styles.name}>Power of Words</h1>
-          <ReactStars
-            value={4.5}
-            activeColor="#5dc269"
-            isHalf={true}
-            size={22}
-            edit={false}
-          />
-          <p className={styles.totalReview}>(142 customers review)</p>
-          <div>
-            <h3 className={styles.price}>$: 24.99</h3>
-            <p className={styles.description}>
-              Lorem ipsum dolor sit amet, consectetur adipisici elit, sed
-              eiusmod tempor incidunt ut labore et dolore magna aliqua. Inmensae
-              subtilitatis, obscuris et malesuada fames. Morbi fringilla
-              convallis sapien, id pulvinar odio volutpat. Ut enim ad minim
-              veniam, quis nostrud exercitation. Quisque placerat facilisis
-              egestas cillum dolore.
-            </p>
-            <p className={styles.stock}>142 in stock</p>
+      <section className={styles.information}>
+        <p className={styles.company}>LOVE BOTTLE</p>
+        <h2 className={styles.name}>Power of Words</h2>
+        <ReactStars
+          value={4.5}
+          activeColor="#5dc269"
+          isHalf={true}
+          size={22}
+          edit={false}
+        />
+        <p className={styles.totalReview}>(142 customer review)</p>
+        <p className={styles.price}>$: 24.99</p>
+        <div className={styles.addContainer}>
+          <p className={styles.earnPoint}>
+            EARN UP TO <span>24 XANHHUB POINTS</span> FOR THIS PURCHASE!
+          </p>
+          <form className={styles.form}>
             <input
+              className={styles.num}
               type="number"
               value={NumItem}
               min="1"
               max="99"
+              pattern="[1-9]{1}[0-9]{1}"
               onChange={(e) => setNumItem(e.target.value)}
             />
-            <button>Add to Cart</button>
-            <div className={styles.categories}>
-              Categories:
-              <span> Drinkware,</span>
-              <span> Travel</span>
-            </div>
-            <div className={styles.tags}>
-              Tags:
-              <span> reusable,</span>
-              <span> glass</span>
-            </div>
-          </div>
+            <button className={styles.addToCart}>ADD TO CART</button>
+          </form>
+          <p className={styles.freeShip}>FREE SHIPPING $50+ | FREE RETURNS</p>
         </div>
-      </div>
+        <div className={styles.icon}>
+          {iconURL.map((url, i) => (
+            <div className={styles.iconImage}>
+              <img src={url} alt={`icon${i}`} />
+            </div>
+          ))}
+        </div>
+        <p className={styles.description}>
+          Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod
+          tempor incidunt ut labore et dolore magna aliqua. Inmensae
+          subtilitatis, obscuris et malesuada fames. Morbi fringilla convallis
+          sapien, id pulvinar odio volutpat. Ut enim ad minim veniam, quis
+          nostrud exercitation. Quisque placerat facilisis egestas cillum
+          dolore.
+        </p>
+        <div className={styles.jump}>
+          <p className={styles.goTo}>GO TO:</p>
+          <a href=" ">Sustainability Features</a>
+          <a href=" ">Product Specifications</a>
+          <a href=" ">Learn More About Love Bottle</a>
+        </div>
+      </section>
     </>
   );
 }
--- src/Pages/ProductPage.js
+++ src/Pages/ProductPage.js
@@ -1,16 +1,15 @@
 import { React, useState } from "react";
 import Layout from "Components/Layout";
 import Information from "Components/Product/Information";
-import SideInformation from "Components/Product/SideInformation";
+import RelatedProduct from "Components/Product/RelatedProduct";
 import styles from "Assets/Stylesheets/SCSS/Pages/ProductPage.module.scss";
-import ReactStars from "react-rating-stars-component";
 export default function ProductPage() {
   return (
     <Layout>
       <div className={styles.product}>
         <div className={styles.main}>
           <Information />
-          <SideInformation />
+          <RelatedProduct />
         </div>
       </div>
     </Layout>
