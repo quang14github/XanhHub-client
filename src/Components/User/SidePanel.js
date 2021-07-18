449d0e00afd8d2bc956c7b4c322354f0d42b003f
quangdm <qduongminh3@gmail.com>
2021-07-18 16:48:48 +0700

    add header to user page, modify tabs in SidePanel

--- src/Components/User/SidePanel.js
+++ src/Components/User/SidePanel.js
@@ -1,29 +1,38 @@
-import React from 'react'
-import styles from 'Assets/Stylesheets/SCSS/Components/SidePanel.module.scss'
-import Avatar from 'Components/Avatar'
-import {useHistory} from 'react-router-dom'
-const tabs=[
-    {name:"Profile",id:"profile"},
-    {name:"History",id:"history"},
-    
-    {name:"Settings",id:"setting"},
-]
-export default function SidePanel() {
-    const url=useHistory()
-    return (
-        <div className={styles.container}>
-            <div className={styles.userInfor}>
-                <Avatar/>
-                <p>User name</p>
-            </div>
-            <div className={styles.tabsContainer}>
-                {tabs.map(e=>(<div key={e.id} className={styles.tabItem}>
-                    <p className={styles.mainTabs}
-                    onClick={()=>{url.push(`/user/${e.id}`)}}
-                    >{e.name}</p>
-                   
-                </div>))}
-            </div>
-        </div>
-    )
+import { React, useState } from "react";
+import styles from "Assets/Stylesheets/SCSS/Components/SidePanel.module.scss";
+import Avatar from "Components/Avatar";
+import logo from "Assets/Images/XanhHub.png";
+import { useHistory } from "react-router-dom";
+const tabs = [
+  { name: "Account details", id: "account" },
+  { name: "Orders", id: "order" },
+  { name: "Logout", id: "logout" },
+];
+export default function SidePanel(props) {
+  const url = useHistory();
+  return (
+    <div className={styles.container}>
+      <div className={styles.userInfor}>
+        <Avatar
+          image="https://cdn.luxe.digital/media/2020/05/18164558/best-short-haircuts-men-high-tight-2-luxe-digital.jpg"
+          alt="avatar"
+        />
+      </div>
+      <div className={styles.tabsContainer}>
+        {tabs.map((e) => (
+          <div key={e.id} className={styles.tabItem}>
+            <p
+              className={styles.mainTabs}
+              onClick={() => {
+                url.push(`/user/${e.id}`);
+                props.changeTitle(e.name);
+              }}
+            >
+              {e.name}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
