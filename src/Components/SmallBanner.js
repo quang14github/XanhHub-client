5aa5468d7621f7396594e17e57ad0d6fd8cb1508
tan <minhtanbg6601@gmail.com>
2021-07-18 16:56:05 +0700

    dev HomePage

--- src/Components/SmallBanner.js
+++ src/Components/SmallBanner.js
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "Assets/Stylesheets/SCSS/Components/SmallBanner.module.scss";
-export default function SmallBanner() {
+export default function SmallBanner({bg}) {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={{backgroundImage:`url("${bg}")`}}>
      <div className={styles.content}>
      <h2>Zero-Watse</h2>
       <p>
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,29 +1,78 @@
-import React from 'react'
-import Layout from 'Components/Layout'
-import Banner from 'Components/Banner'
-import SmallBanner from 'Components/SmallBanner'
-import ProductList from 'Components/ProductList'
-import styles from 'Assets/Stylesheets/SCSS/Pages/HomePage.module.scss'
-import IntroductionBanner from 'Components/IntroductionBanner'
+import React from "react";
+import Layout from "Components/Layout";
+import Banner from "Components/Banner";
+import SmallBanner from "Components/SmallBanner";
+import styles from "Assets/Stylesheets/SCSS/Pages/HomePage.module.scss";
+import IntroductionBanner from "Components/IntroductionBanner";
+import friendly from "Assets/Images/friend.jpg";
+import zeroWatse from "Assets/Images/zero-watse.jpg";
+import recycle from "Assets/Images/recycle.jpg";
+import productImg from "Assets/Images/product.png";
+import shipImg from "Assets/Images/ship.png";
+import recImg from "Assets/Images/recommend.png";
+
 export default function HomePage() {
-    return (
-        <Layout>
-                <Banner/>
-                <IntroductionBanner/>
-                
-                <div className={styles.groupBanners}>
-                    <div className={styles.content}>
-                    <SmallBanner/>
-                    <SmallBanner/>
-                    <SmallBanner/>
-                    </div>
-                </div>
-                <div className={styles.groupProducts}>
-                    <h1 className={styles.title}>Our Products</h1>
-                   
-                    
-                   
-                </div>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <Banner />
+      <IntroductionBanner />
+
+      <About />
+      <div style={{backgroundColor:'var(--secondary-background-trans)'}} className={styles.brandName}>
+            <h1>Verified Products</h1>
+        </div>
+      <div className={styles.groupBanners}>
+        <SmallBanner bg={recycle} />
+        <SmallBanner bg={zeroWatse} />
+        <SmallBanner bg={friendly} />
+      </div>
+
+      <div className={styles.loginSuggest}>
+          <h3>Feel Interested?</h3>
+          <button className={`button ${styles.buttonLogin}`}>Try IT</button>
+      </div>
+
+      <footer className={styles.homeFooter}>
+        <h1>2021 - xanhhub</h1>
+      </footer>
+    </Layout>
+  );
 }
+
+const aboutList=[
+    {
+        name:"Products",
+        img:productImg,
+        des:["We have hundreds of products from many suppliers.","You can search for products and make orders online.","If your want to become a supplier, just contact us!"]
+    },
+    {
+        name:"Recommendation",
+        img:recImg,
+        des:["Our Recommendation system make it easier than ever to shopping."," You can buy what you need with least decisions. ","The more products you buy, the more we can suggest!"]
+    },
+    {
+        name:"Shipping",
+        img:shipImg,
+        des:["Once your order is made, our shipping providers will make their best to delivery to you as soon as possible.","If you are not satisfied, We are welcome to receive it back."]
+    }
+]
+const About = () => {
+    const [cur,setCur]=React.useState(0)
+  return (
+    <div className={styles.about}>
+        <div className={styles.brandName}>
+            <h1>Green Purchasing Flatform</h1>
+        </div>
+      <div className={styles.tech}>
+       
+        {aboutList.map((e,i)=>( <div onClick={()=>{setCur(i)}} key={e.name} className={`${styles.techItem} ${cur===i?styles.on:''}`}>
+          <img alt={e.name} src={e.img} />
+          <p>{e.name}</p>
+        </div>))}
+      </div>
+      <div className={styles.describe} key={"des-"+cur}>
+    {aboutList[cur].des.map((e,i)=><p key={i}>{e}</p>)}
+      </div>
+    </div>
+  );
+};
