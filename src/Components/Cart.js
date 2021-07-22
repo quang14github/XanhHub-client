56847169d6488803e19c273aa6aea1e9afb391ea
quangdm <qduongminh3@gmail.com>
2021-07-22 23:28:12 +0700

    hard code Cart, responsive

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,32 +1,16 @@
 import React from "react";
 import styles from "Assets/Stylesheets/SCSS/Components/Cart.module.scss";
 import { useState } from "react";
-const order = [
-  // {
-  //   shop: "3ccases.vn",
-  //   product: [
-  //     {
-  //       image: "https://cf.shopee.vn/file/0d8e38c9ba0da4b2ff60a6a51f6bd737",
-  //       name: "Vỏ bảo vệ hộp sạc tai nghe silicon mềm họa tiết gấu hoạt hình Hàn Quốc 2020 kèm dây treo cho Samsung Galaxy Buds +",
-  //       price: "$58.000",
-  //       quantity: "1",
-  //     },
-  //     {
-  //       image: "https://cf.shopee.vn/file/0d8e38c9ba0da4b2ff60a6a51f6bd737",
-  //       name: "Vỏ bảo vệ hộp sạc tai nghe silicon mềm họa tiết gấu hoạt hình Hàn Quốc 2020 kèm dây treo cho Samsung Galaxy Buds +",
-  //       price: "$58.000",
-  //       quantity: "1",
-  //     },
-  //   ],
-  // },
-];
+import { useSelector } from "react-redux";
+import useCompany from "./useCompany";
+const Entities = require("html-entities").AllHtmlEntities;
+
+const entities = new Entities();
 export default function Cart(props) {
-  const [quantity, setQuantity] = useState(
-    [[1, 1]],
-    (shopNumber, productNumber, change) => {
-      quantity[shopNumber][productNumber] =
-        parseInt(quantity[shopNumber][productNumber]) + change;
-    }
+  const userCart = useSelector((state) => state.cart.cart);
+  const companys = useCompany();
+  const [Quantity, setQuantity] = useState(
+    userCart.map((e) => parseInt(e.count))
   );
   return (
     <div className={styles.container}>
@@ -35,68 +19,92 @@ export default function Cart(props) {
           <input type="checkbox" />
           <p>Product</p>
         </div>
-        <p>Price</p>
         <p>Quantity</p>
         <p>Subtotal</p>
       </div>
-      {order.map((e, shopNumber) => (
-        <div className={styles.order}>
-          <div className={styles.shop}>
+      {userCart.map((e, index) => (
+        <div className={styles.product}>
+          <div className={styles.info}>
             <input type="checkbox" />
-            <p>{e.shop}</p>
-            <div className={styles.barButtonGroup}>
-              <button className={styles.chat}>Chat</button>
-              <button className={styles.viewShop}>View Shop</button>
+            <div className={styles.image}>
+              <img src={e.img} alt="product" />
+            </div>
+            <div className={styles.name_group}>
+              <p className={styles.shop_name}>
+                {entities.decode(companys[e.supCode].name)}
+              </p>
+              <p className={styles.product_name}>{e.name}</p>
             </div>
           </div>
-          {e.product.map((product, productNumber) => (
-            <>
-              <div className={styles.product}>
-                <div className={styles.info}>
-                  <input type="checkbox" />
-                  <div className={styles.image}>
-                    <img src={product.image} alt="product" />
-                  </div>
-                  <p className={styles.name}>{product.name}</p>
-                </div>
-                <div className={styles.subInfo}>
-                  <p className={styles.price}>{product.price}</p>
-                  <div className={styles.selectQuantity}>
-                    <span
-                      onClick={() => {
-                        setQuantity(shopNumber, productNumber, -1);
-                      }}
-                    >
-                      {"\u2212"}
-                    </span>
-                    <input
-                      className={styles.quantity}
-                      type="number"
-                      value={product.quantity}
-                      min="1"
-                      max="99"
-                    />
-                    <span
-                      onClick={() => {
-                        setQuantity(shopNumber, productNumber, 1);
-                      }}
-                    >
-                      +
-                    </span>
-                  </div>
-                  <p className={styles.subTotal}>$58.000</p>
-                </div>
-              </div>
-              <div className={styles.buttonGroup}>
-                <button className={styles.mainButton}>Remove </button>
-              </div>
-            </>
-          ))}
+          <div className={styles.subInfo}>
+            <div className={styles.selectQuantity}>
+              <span
+                onClick={() =>
+                  setQuantity(
+                    Quantity.map((element, elementIndex) => {
+                      if (elementIndex === index && element > 1)
+                        return element - 1;
+                      return element;
+                    })
+                  )
+                }
+              >
+                {"\u2212"}
+              </span>
+              <input
+                className={styles.quantity}
+                type="number"
+                value={Quantity[index]}
+                min="1"
+                max="99"
+                onChange={(e) =>
+                  setQuantity(
+                    Quantity.map((element, elementIndex) => {
+                      if (
+                        elementIndex === index &&
+                        e.target.value < 100 &&
+                        e.target.value > 0
+                      )
+                        return Math.floor(e.target.value);
+                      return element;
+                    })
+                  )
+                }
+              />
+              <span
+                onClick={() =>
+                  setQuantity(
+                    Quantity.map((element, elementIndex) => {
+                      if (elementIndex === index && element < 99)
+                        return element + 1;
+                      return element;
+                    })
+                  )
+                }
+              >
+                +
+              </span>
+            </div>
+            <p className={styles.subTotal}>
+              ${(Quantity[index] * e.price).toFixed(2)}
+            </p>
+            <div className={styles.buttonGroup}>
+              <button className={styles.mainButton}>Remove </button>
+            </div>
+          </div>
         </div>
       ))}
-      <button onClick={() => props.checkout(!props.toCheckout)}>
-        CHECKOUT!!!
-      </button>
+      <div className={styles.checkoutArea}>
+        <div>
+          Total cost:
+          {Quantity.reduce(function (total, currentProduct, currentIndex) {
+            return total + currentProduct * userCart[currentIndex].price;
+          }, 0).toFixed(2)}
+        </div>
+        <button onClick={() => props.checkout(!props.toCheckout)}>
+          CHECKOUT!!!
+        </button>
+      </div>
     </div>
   );
 }
