0b17cece98cc9b8b5fbfb56dc5d521609340d77f
quangdm <qduongminh3@gmail.com>
2021-07-25 08:26:14 +0700

    get products by recommender system

--- src/Components/Product/RelatedProduct.js
+++ src/Components/Product/RelatedProduct.js
@@ -2,45 +2,12 @@ import React from "react";
 import styles from "Assets/Stylesheets/SCSS/Components/RelatedProduct.module.scss";
 //import ProductInformation from "./Information";
 import ProductList from "Components/ProductList";
-// const products = [
-//   {
-//     url: "https://d13wriz42ny3t5.cloudfront.net/production/2017/08/23230802/love-bottle-family-recycled-glass-water-bottle-300x300.jpg",
-//     name: "All Clear Water Bottle",
-//     price: "$24.99",
-//   },
-  // {
-  //   url: "https://d13wriz42ny3t5.cloudfront.net/production/2017/08/23230802/love-bottle-family-recycled-glass-water-bottle-300x300.jpg",
-  //   name: "Honey Water Bottle",
-  //   price: "$24.99",
-  // },
-  // {
-  //   url: "https://d13wriz42ny3t5.cloudfront.net/production/2017/08/23230802/love-bottle-family-recycled-glass-water-bottle-300x300.jpg",
-  //   name: "Family Water Bottle",
-  //   price: "$24.99",
-  // },
-  // {
-  //   url: "https://d13wriz42ny3t5.cloudfront.net/production/2017/08/23230802/love-bottle-family-recycled-glass-water-bottle-300x300.jpg",
-  //   name: "Peace Water Bottle",
-  //   price: "$24.99",
-  // },
-  // {
-  //   url: "https://d13wriz42ny3t5.cloudfront.net/production/2017/08/23230802/love-bottle-family-recycled-glass-water-bottle-300x300.jpg",
-  //   name: "Mandala Water Bottle",
-  //   price: "$24.99",
-  // },
-  // {
-  //   url: "https://d13wriz42ny3t5.cloudfront.net/production/2017/08/23230802/love-bottle-family-recycled-glass-water-bottle-300x300.jpg",
-  //   name: "Love Our Earth Water Bottle",
-  //   price: "$24.99",
-  // },
-//];
-export default function RelatedProduct({cat,SID}) {
-  
+export default function RelatedProduct({ cat, SID }) {
   return (
     <div className={styles.container}>
       <h3 className={styles.title}>RELATED PRODUCTS</h3>
-    
-      <ProductList SID={SID} category={cat}/>
+
+      <ProductList SID={SID} type="CS" limit="10" />
     </div>
   );
 }
--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -3,56 +3,98 @@ import styles from "Assets/Stylesheets/SCSS/Components/ProductList.module.scss";
 import sample from "Assets/JS/products";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Link } from "react-router-dom";
-import "swiper/components/navigation/navigation.min.css"
+import "swiper/components/navigation/navigation.min.css";
 
-import SwiperCore, {
-    Navigation
-  } from 'swiper/core';
-  
-  // install Swiper modules
-  SwiperCore.use([Navigation]);
-  
-export default function ProductList({ SID, pagination = false ,category=null}) {
-  const [product,setProducts]=React.useState(null)
-  React.useEffect(async()=>{
-    if(category){
-      const data=await fetch('http://207.46.145.28/v1/product/all').then(res=>res.json())
-      const arr=data.filter(e=>e.CATEGORY===category).filter(e=>Math.random()<.5)
-      
-      for(let i=0;i<arr.length;i++){
-          let index=Math.floor(Math.random()*i)
-          const temp=arr[i]
-          arr[i]=arr[index]
-          arr[index]=temp;
+import SwiperCore, { Navigation } from "swiper/core";
+
+// install Swiper modules
+SwiperCore.use([Navigation]);
+
+export default function ProductList({
+  SID,
+  USER_ID,
+  pagination = false,
+  category = null,
+  type = null,
+  limit = null,
+}) {
+  const [product, setProducts] = React.useState(null);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  React.useEffect(async () => {
+    if (type === "CS") {
+      const data = await fetch(
+        `http://207.46.145.28/v1/recommend/product?TYPE=${type}&LIMIT=${limit}&PRODUCT_ID=${SID}`
+      ).then((res) => res.json());
+      const products = [];
+      for (let i = 0; i < data.CS.length; i++) {
+        let newProduct = fetch(
+          `http://207.46.145.28/v1/product/byid?id=${data.CS[i]}`
+        ).then((res) => res.json());
+        products.push(newProduct);
       }
-      setProducts(arr)
-      
-     
+      for (let i = 0; i < products.length; i++) {
+        products[i] = await products[i];
+      }
+      setProducts(products);
+    } else if (type === "CF") {
+      const data = await fetch(
+        `http://207.46.145.28/v1/recommend/product?TYPE=${type}&USER_ID=${USER_ID}`
+      ).then((res) => res.json());
+      const products = [];
+      const minNumberOfProduct = Math.min(data.CF.length, parseInt(limit));
+      console.log(minNumberOfProduct);
+      for (let i = 0; i < minNumberOfProduct; i++) {
+        let newProduct = fetch(
+          `http://207.46.145.28/v1/product/byid?id=${data.CF[i]}`
+        ).then((res) => res.json());
+        products.push(newProduct);
+      }
+      for (let i = 0; i < products.length; i++) {
+        products[i] = await products[i];
+      }
+      setProducts(products);
+    } else if (category) {
+      const data = await fetch("http://207.46.145.28/v1/product/all").then(
+        (res) => res.json()
+      );
+      const arr = data
+        .filter((e) => e.CATEGORY === category)
+        .filter((e) => Math.random() < 0.5);
+
+      for (let i = 0; i < arr.length; i++) {
+        let index = Math.floor(Math.random() * i);
+        const temp = arr[i];
+        arr[i] = arr[index];
+        arr[index] = temp;
+      }
+      setProducts(arr);
     }
-  },[category,SID])
+  }, [category, SID]);
   return (
     <div className={styles.container}>
       <Swiper
         spaceBetween={5}
         navigation={{
-            nextEl:'.swiper-button-next',
-            prevEl:'.swiper-button-prev'
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
         }}
         slidesPerView={"auto"}
         className={styles.Swiper}
-        
       >
-        {product?product.map((e) => (
-          <SwiperSlide key={e.id}>
-            <ProductItem data={e} />
-          </SwiperSlide>
-        )):sample.map((e) => (
-          <SwiperSlide key={e.id}>
-            <SampleProduct data={e} />
-          </SwiperSlide>
-        ))}
+        {product
+          ? product.map((e) => (
+              <SwiperSlide key={e.id}>
+                <ProductItem data={e} />
+              </SwiperSlide>
+            ))
+          : sample.map((e) => (
+              <SwiperSlide key={e.id}>
+                <SampleProduct data={e} />
+              </SwiperSlide>
+            ))}
+
         <div class="swiper-button-next"> {">"}</div>
-     <div class="swiper-button-prev">{"<"}</div>
+        <div class="swiper-button-prev">{"<"}</div>
       </Swiper>
     </div>
   );
@@ -71,10 +113,11 @@ const SampleProduct = ({ data }) => {
 const ProductItem = ({ data }) => {
   return (
     <Link to={`/product/${data.SID}`}>
-    <div title={data.post_title} className={styles.product}>
-      <img src={data.img} alt={data.NAME} />
-      <p className={styles.price}>$:{parseFloat(data.PRICE).toFixed(2)}</p>
-      <p>{data.NAME.substring(0, 20)}</p>
-    </div></Link>
+      <div title={data.post_title} className={styles.product}>
+        <img src={data.img} alt={data.NAME} />
+        <p className={styles.price}>$:{parseFloat(data.PRICE).toFixed(2)}</p>
+        <p>{data.NAME.substring(0, 20)}</p>
+      </div>
+    </Link>
   );
 };
